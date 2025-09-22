04ff658af5372b46044ad5cf8e8c9f9145030834
Carlos Gutiérrez Orjuela <cg629271@gmail.com>
2025-09-22 11:04:50 -0500

    Create ask-a-number.cpp

---
+++ ask-a-number.cpp
@@ -0,0 +1,13 @@
+//tipos de datos basicos en c++
+
+#include <iostream>     
+using namespace std;
+int main(){     
+    float numero;
+    cout<<"digita un numero : ";
+    cin>>numero;//asi le asignamos lo que digite el usuario a la variable
+    
+    cout<<"\n El numero que digito es: " << numero ;//el \n es para hacer salto de linea igual que el endline
+    
+    return 0;
+}
