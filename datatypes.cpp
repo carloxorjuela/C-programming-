0f12696328fb7a16a03c0a6640081324adcdd091
Carlos Gutiérrez Orjuela <cg629271@gmail.com>
2025-09-22 10:53:31 -0500

    Create datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,15 @@
+//tipos de datos basicos en c++
+
+#include <iostream>     
+using namespace std;
+int main(){
+    int entero = 15;
+    float flotante =10.45;
+    double mayor = 16.456;
+    char letra = 'C';
+    cout<<entero<<endl;
+    cout<<flotante<<endl;
+    cout<<mayor<<endl;
+    cout<<letra<<endl;
+    return 0;
+}
